6bb5437264fa8badfba70c027c5d2f595d6a6bb4
harries4206 <harries4206@gmail.com>
2024-06-27 17:43:25 +0800

    add dots for infinite carousel

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const items = document.querySelectorAll(".slider .list .item");
 const dots = document.querySelectorAll(".slider .dots li");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-
+const second = 2000;
 let active = 0;
 let lengthItem = items.length - 1;
-
+let autoNextSlider; // 輪播interval
 prev.addEventListener("click", () => {
-  console.log("prev click");
   if (active - 1 < 0) active = lengthItem;
   else active -= 1;
   reloadSlider();
@@ -19,10 +18,11 @@ next.addEventListener("click", () => {
   else active += 1;
   reloadSlider();
 });
-// 輪播interval
-let autoNextSlider = setInterval(() => {
+
+autoNextSlider = setInterval(() => {
   next.click();
-}, 2000);
+}, second);
+
 // 移動圖片
 const reloadSlider = () => {
   // offsetLeft 與 relative 的距離
@@ -34,9 +34,10 @@ const reloadSlider = () => {
   dots[active].classList.add("active");
   // 點擊時要重新設定interval 重新計時
   clearInterval(autoNextSlider);
+
   autoNextSlider = setInterval(() => {
     next.click();
-  }, 2000);
+  }, second);
 };
 
 dots.forEach((li, key) => {
--- index3.js
+++ index3.js
@@ -1,47 +1,77 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // 往右移動 移除第一個img 新增第一個img在最後面
-  // 取得全部圖片src
+  // 1. 取得圖片列表與圖片container
   const carouselTrack = document.querySelector(".carousel-track");
   const carouselImages = document.querySelectorAll(".carousel-image");
   const images = Array.from(carouselTrack.children);
+  const dots = document.querySelectorAll(".carousel .dots li");
+  // 2. 取得圖片寬度
   const imageWidth = images[0].getBoundingClientRect().width + 10; // 圖片寬度加上間距
   const originalImagesCount = images.length;
-  let currentIndex = 2;
-  let prevIndex = 0;
+  // 3. 利用圖片列表找出實際呈現的圖片
+  const realImages = [];
+  carouselImages.forEach((node, index) => {
+    if (!node.classList.contains("clone")) {
+      realImages.push({ element: node, index: index });
+    }
+  });
+  // 4. 定義開始圖片位置
+  let currentIndex = realImages[0].index;
+  let prevIndex = currentIndex;
+  const lastImage = realImages.at(-1).index;
+  // 5. 利用translate3d 移動到第一張實際圖片位置
+  carouselTrack.style.transform = `translate3d(-${
+    imageWidth * realImages[0].index
+  }px,0,0)`;
+
+  function moveDot(index, type = "") {
+    if (type === "remove") {
+      dots[index].classList.remove("active");
+      return;
+    }
+    dots[index].classList.add("active");
+  }
 
   function moveCarousel() {
     prevIndex = currentIndex;
-    currentIndex = (currentIndex + 1) % originalImagesCount;
-    if (currentIndex === 6) {
+    currentIndex = currentIndex + 1;
+    //  往後到clone的圖片後馬上回到第一張實際圖片位置
+    if (currentIndex === lastImage + 1) {
       setTimeout(() => {
-        // carouselTrack.appendChild(carouselImages[prevIndex]);
         carouselTrack.style.transition = "none";
         carouselTrack.style.scrollBehavior = "none";
-        carouselTrack.style.transform = `translate3d(-620px,0,0)`;
-        carouselImages[2].classList.add("active");
-        currentIndex = 2;
-      }, 300);
+        carouselTrack.style.transform = `translate3d(-${
+          imageWidth * realImages[0].index
+        }px,0,0)`;
+        carouselImages[realImages[0].index].classList.add("active");
+        moveDot(realImages.findIndex((i) => i.index === realImages[0].index));
+        moveDot(
+          realImages.findIndex((i) => i.index === prevIndex),
+          "remove"
+        );
+        carouselImages[lastImage + 1].classList.remove("active"); // 刪除clone圖片的active
+        currentIndex = realImages[0].index;
+      }, 500);
     }
+    // 移動到實際圖片位置
     carouselTrack.style.transition = "transform 0.5s ease ";
-    // carouselTrack.style.scrollBehavior = "smooth";
     carouselTrack.style.transform = `translate3d(-${
       imageWidth * currentIndex
     }px,0,0)`;
     carouselImages[currentIndex].classList.add("active");
+    console.log("realImages", realImages);
+    if (realImages.findIndex((i) => i.index === currentIndex) !== -1) {
+      moveDot(realImages.findIndex((i) => i.index === currentIndex));
+      moveDot(
+        realImages.findIndex((i) => i.index === prevIndex),
+        "remove"
+      );
+    }
+
     setTimeout(() => {
       carouselTrack.style.transition = "";
       carouselTrack.style.scrollBehavior = "none";
       carouselImages[prevIndex].classList.remove("active");
     }, 500);
-    console.log("currentIndex", currentIndex);
-
-    // if (currentIndex > 4) {
-    //   setTimeout(() => {
-    //     carouselTrack.style.transition = "none";
-    //     carouselTrack.style.transform = ``;
-    //     currentIndex = 0;
-    //   }, 500); // 確保過渡結束
-    // }
   }
 
   setInterval(moveCarousel, 2000);
